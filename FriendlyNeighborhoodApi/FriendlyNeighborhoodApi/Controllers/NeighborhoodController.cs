c53b27791eaa987518ddd07b2897dfcf7b8414b9
fridrik <fbolafsson@gmail.com>
2016-09-13 23:25:33 +0000

    Maybe not so misguided :)

---
+++ FriendlyNeighborhoodApi/FriendlyNeighborhoodApi/Controllers/NeighborhoodController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FriendlyNeighborhoodApi.Models;
+
+namespace FriendlyNeighborhoodApi.Controllers
+{
+    public class NeighborhoodController : ApiController
+    {
+        public HttpResponseMessage Get()
+        {
+            var message = new GreatModel { Id = 1, Message = "Well, well, well!"};
+            return Request.CreateResponse(HttpStatusCode.OK, message);
+        }
+    }
+}
